dcb80272fb9207963a5fae70be339e14cb236adc
ocean.o <ocean.o@kakaocorp.com>
2022-11-14 13:10:31 +0900

    Initial Commit

---
+++ TodayworkoutDone/TodayworkoutDoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayworkoutDoneApp.swift
+//  TodayworkoutDone
+//
+//  Created by Sihoon Oh on 2022/11/14.
+//
+
+import SwiftUI
+
+@main
+struct TodayworkoutDoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
