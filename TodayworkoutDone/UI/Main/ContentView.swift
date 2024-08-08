dbcd8fa167ddd194adae614f1f87f4ac0e800398
sihon321 <sihon321@gmail.com>
2024-08-08 09:41:57 +0900

    - 기존 clean architecture 코드 제거

--- TodayworkoutDone/UI/Main/ContentView.swift
+++ TodayworkoutDone/UI/Main/ContentView.swift
@@ -11,11 +11,6 @@ import ComposableArchitecture
 
 struct ContentView: View {
     @Environment(\.presentationMode) var presentationmode
-    private let container: DIContainer
-    
-    init(container: DIContainer) {
-        self.container = container
-    }
     
     var body: some View {
         GeometryReader { proxy in
@@ -28,7 +23,6 @@ struct ContentView: View {
                     )) {
                         HomeReducer()
                     })
-            .inject(container)
             .ignoresSafeArea(.all, edges: .bottom)
         }
     }
@@ -37,6 +31,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     
     static var previews: some View {
-        ContentView(container: .preview)
+        ContentView()
     }
 }
