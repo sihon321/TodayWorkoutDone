416be0711b1ba27f4fca758de7f8fa681f32f229
sihon321 <sihon321@gmail.com>
2024-08-10 17:55:58 +0900

    Revert "- 기존 clean architecture 코드 제거"
    This reverts commit dbcd8fa167ddd194adae614f1f87f4ac0e800398.

---
+++ TodayworkoutDone/System/AppEnvironment.swift
@@ -0,0 +1,84 @@
+//
+//  AppEnvironment.swift
+//  TodayworkoutDone
+//
+//  Created by Sihoon Oh on 2023/05/30.
+//
+
+import Foundation
+import Combine
+
+struct AppEnvironment {
+    let container: DIContainer
+    let systemEventsHandler: SystemEventHandler
+}
+
+extension AppEnvironment {
+    static func bootstrap() -> AppEnvironment {
+        let appState = LegacyStore<AppState>(AppState())
+        
+        let webRepositories = configuredWebRepositories()
+        let dbRepositories = configuredDBRepositories()
+        
+        let interactors = configuredInteractors(appState: appState,
+                                                dbRepositories: dbRepositories,
+                                                webRepositories: webRepositories)
+        let diContainer = DIContainer(appState: appState, interactors: interactors)
+        let systemEventsHandler = RealSystemEventsHandler()
+        
+        return AppEnvironment(container: diContainer, systemEventsHandler: systemEventsHandler)
+    }
+    
+    private static func configuredWebRepositories() -> DIContainer.WebRepositories {
+        let categoryRepository = RealCategoryWebRepository()
+        let workgoutsRepository = RealWorkoutWebRepository()
+        
+        return .init(workoutsRepository: workgoutsRepository, categoryRepository: categoryRepository)
+    }
+    
+    private static func configuredDBRepositories() -> DIContainer.DBRepositories {
+        let persistentStore = CoreDataStack(version: CoreDataStack.Version.actual)
+        let categoryRepository = RealCategoryDBRepository(persistentStore: persistentStore)
+        let workgoutsRepository = RealWorkoutDBRepository(persistentStore: persistentStore)
+        let routineRepository = RealRoutineDBRepository(persistentStore: persistentStore)
+        
+        return .init(workoutsRepository: workgoutsRepository,
+                     categoryRepository: categoryRepository,
+                     routineRepository: routineRepository)
+    }
+    
+    private static func configuredInteractors(appState: LegacyStore<AppState>,
+                                              dbRepositories: DIContainer.DBRepositories,
+                                              webRepositories: DIContainer.WebRepositories
+    ) -> DIContainer.Interactors {
+        let workoutInteractor = RealWorkoutInteractor(
+            webRepository: webRepositories.workoutsRepository,
+            dbRepository: dbRepositories.workoutsRepository,
+            appState: appState
+        )
+        let categoryInteractor = RealCategoryInteractor(
+            webRepository: webRepositories.categoryRepository,
+            dbRepository: dbRepositories.categoryRepository,
+            appState: appState)
+        let routineInteractor = RealRoutinesInteractor(dbRepository: dbRepositories.routineRepository)
+        let healthkitInteractor = RealHealthKitInteractor()
+        
+        return .init(workoutInteractor: workoutInteractor,
+                     categoryInteractor: categoryInteractor,
+                     routineInteractor: routineInteractor,
+                     healthkitInteractor: healthkitInteractor)
+    }
+}
+
+extension DIContainer {
+    struct WebRepositories {
+        let workoutsRepository: WorkoutWebRepository
+        let categoryRepository: CategoryWebRepository
+    }
+    
+    struct DBRepositories {
+        let workoutsRepository: WorkoutDBRepository
+        let categoryRepository: CategoryDBRepository
+        let routineRepository: RoutineDBRepository
+    }
+}
