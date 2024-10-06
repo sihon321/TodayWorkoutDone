08def05adacc8844ab523d28f799ea845d1b81b0
sihon321 <sihon321@gmail.com>
2024-10-07 01:57:04 +0900

    [TOD-17] clean architecture 적용되었던 부분 TCA로 변경
    - MyRoutine들을 db에서 가져오도록 수정 후 동작 tca적용

---
+++ TodayworkoutDone/UI/WorkOut/MyRoutineView.swift
@@ -0,0 +1,52 @@
+//
+//  MyWorkoutView.swift
+//  TodayworkoutDone
+//
+//  Created by Sihoon Oh on 2022/12/25.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+@Reducer
+struct MyRoutineReducer {
+    @ObservableState
+    struct State: Equatable {
+        var text: String = ""
+        var myRoutines: [MyRoutine] = []
+        var selectedRoutine: MyRoutine?
+    }
+    
+    enum Action {
+        case touchedMyRoutine(MyRoutine)
+        case touchedEditMode(MyRoutine)
+    }
+}
+
+struct MyRoutineView: View {
+    @Bindable var store: StoreOf<MyRoutineReducer>
+    @ObservedObject var viewStore: ViewStoreOf<MyRoutineReducer>
+    
+    init(store: StoreOf<MyRoutineReducer>) {
+        self.store = store
+        self.viewStore = ViewStore(store, observe: { $0 })
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("my Routine")
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(viewStore.myRoutines) { myRoutine in
+                        Button(action: {
+                            viewStore.send(.touchedMyRoutine(myRoutine))
+                        }) {
+                            MyWorkoutSubview(store: store, myRoutine: myRoutine)
+                        }
+                    }
+                }
+            }
+        }
+        .padding([.leading, .trailing], 15)
+    }
+}
